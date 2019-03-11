26baaee3dbf447e685737cb565f61d66fb42d5d4
Tim Sawtell <timothy.sawtell@gmail.com>
2019-03-11 18:11:42 +1100

    altered use case

--- App.js
+++ App.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
-import { Text, View, Button, ActivityIndicator } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      jsonGets: 0,
+      gets: 0,
       leakyDataPressed: false
     }
   }
 
-  getJSONText = () => {
-    fetch('https://raw.githubusercontent.com/timsawtell/react-native-fetch-memory-leak/master/package.json').then((response) => {
-      return response.json()
-    }).then((responseAsJSON) => {
-      this.setState({
-        jsonGets: this.state.jsonGets + 1
-      })
-      setTimeout(this.getJSONText, 100);
-    })
-  }
-
-  getLeakyData = () => {
-    this.setState({
-      leakyDataPressed: true
-    })
-    fetch('http://ipv4.download.thinkbroadband.com/5MB.zip').then((response) => {
-      this.setState({
-        leakyDataPressed: false
-      })
-      setTimeout(this.getJSONText, 100);
-    })
-  }
-
   componentDidMount() {
-    this.getJSONText()
+    this.hitAnAPI();
+  }
+  hitAnAPI = () => {
+    this.getMoviesFromApiAsync().then(this.doSomethingWithData);
+  }
+
+  getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then(this.processJSON)
+      .then(this.processMovies)
+  }
+
+  processJSON = (response) => response.json()
+
+  processMovies = (responseJson) => {
+    return responseJson.movies;
+  }
+
+  doSomethingWithData = (movies) => {
+    this.setState({
+      gets: this.state.gets + 1
+    });
+    console.log(JSON.stringify(movies))
+    setTimeout(this.hitAnAPI, 300) // start all over again
   }
 
   render() {
@@ -46,10 +46,7 @@ export default class App extends Component {
         flex: 1
       }}>
         <Text>Memory Leak Demo.</Text>
-        <Text>JSON Text Gets: {this.state.jsonGets}</Text>
-        <Button onPress={this.getLeakyData} title="Get Leaky Data" disabled={this.state.leakyDataPressed} />
-        <ActivityIndicator animating={this.state.leakyDataPressed} />
-        <Text>The reason this leaks is because the programmer isn't doing anything with the response. The reason why the `JSON Text Gets` do not leak is because the value is read (via .json()) and this in turn signals to the RCTBlobManager to delete that data (with my modification to `RCTFileReaderModule`). If you don't want fetch results to stay in memory forever, you'd best read the response via .json() or .text() .</Text>
+        <Text>JSON object Gets: {this.state.gets}</Text>
       </View>
     );
   }
